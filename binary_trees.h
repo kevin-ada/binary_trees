400ab731a984babb305538d39149e5edbfbef2b6
kim-thuita <kevinkoech265@gmail.com>
2023-08-02 02:50:07 +0300

    fixing task-1 issues in the .h and main.c

--- binary_trees.h
+++ binary_trees.h
@@ -1,11 +1,8 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
-
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>;
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -29,5 +26,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /*Working with mandatory tasks */
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
