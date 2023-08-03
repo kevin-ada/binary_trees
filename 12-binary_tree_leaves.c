f7226e2b9f89aba744b700973895b467d0568d58
kevin-ada <kevinkoech265@gmail.com>
2023-08-04 00:02:39 +0300

    leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_leaves - function that returns num of leaves
+ *
+ *@tree: tree that checks
+ *
+ *Return: number of leaves
+ */
+size_t binary_tree-leaves(const binary_tree-t *tree)
+{
+	size_t leaf = 0;
+
+	size_t x = 0;
+
+	size_t y = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		x = binary_tree_leaves(tree->left);
+		y = binary_tree_leaves(tree->right);
+
+		leaf = x + y;
+
+		return ((!x && !y) ? leaf + 1 : leaf + 0);
+	}
+}
+
